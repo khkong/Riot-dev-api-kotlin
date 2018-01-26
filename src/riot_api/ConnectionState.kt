971f242fb04b076661ac5623949e9152acb355e8
khkong <khkong0928@naver.com>
2018-01-26 18:25:37 +0900

    Add global variables for each API.

---
+++ src/riot_api/ConnectionState.kt
@@ -0,0 +1,14 @@
+package riot_api
+
+const val CHAMPION_MASTERY_V3 = 1;
+const val CHAMPION_V3 = 2;
+const val LEAGUE_V3 = 4;
+const val LOL_STATIC_DATA_V3 = 8;
+const val LOL_STATUS_V3 = 16;
+const val MATCH_V3 = 32;
+const val SPECTATOR_V3 = 64;
+const val SUMMONER_V3 = 128;
+const val THIRD_PARTY_CODE_V3 = 256;
+const val TOURNAMENT_STUB_V3 = 512;
+const val TOURNAMENT_V3 = 1024;
+
